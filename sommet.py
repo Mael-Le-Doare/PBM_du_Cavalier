345156652f3b4ce265299a3a0a18884c52af3166
Le Doaré <mael.ledoare@etudiant.univ-rennes.fr>
2026-02-09 14:50:58 +0100

    renommage

---
+++ sommet.py
@@ -0,0 +1,73 @@
+# On définit une classe sommet
+class Sommet:
+    # nom du sommet
+    nom : str = ""
+    # dictionnaire des sommet adjacents (la clef etant le sommet et la valeur designe le poids de l'arrette)
+    sommetAdjacents : dict = {}
+    # le sommet a t'il ete visite ?
+    estVisite : bool = False
+    # degré du graph
+    degre : int = 0
+    # boucles du graphe
+    boucle : int =0
+
+    def __init__(self, n : str, sA : list) :
+        # on assigne le nom et les sommet adjacents passés en parametre
+        self.nom = n
+        self.sommetAdjacents=sA
+
+        # pour chacun des sommets rencontontrés on incrémente le degré du sommet
+        self.initDegre()
+    
+    def __str__(self):
+        # affichage de notre objet sommet
+        return f"{self.nom}"
+    
+    
+    def afficherAdjacents(self):
+        chaineSortie : str = f"Sommets adjacents de {self.nom} ="
+        # affichage des sommets adjacents
+        for sommet in self.sommetAdjacents.items():
+            chaineSortie+=f" | sommet : {sommet[0]} poids : {(sommet[1])}"
+        print(chaineSortie+" |")
+    
+    def retourneDegre(self)->int:
+        #renvoie la valeur (entier) du degre de notre sommet
+        return self.degre
+    
+    def retourneBoucle(self)->int:
+        return self.boucle
+    
+    def initAdjacents(self, dico : dict):
+        # methode pour actualiser le dictionnaire des sommets adjacents
+        self.sommetAdjacents.update(dico)
+        self.initDegre()
+    
+    def initDegre(self)->int:
+        #initianalise les degre du sommet
+        for sommets in self.sommetAdjacents:
+            self.degre +=1
+            #si l'on rencontre une boucle le degré de notre sommet est incrémenté de 2
+            if sommets.nom==self.nom:
+                self.degre +=1
+                #on incremente egalement le nombre de boucle de notre sommet
+                self.boucle +=1
+    
+    def estIsole(self)->bool:
+        return self.sommetAdjacents=={}
+
+
+
+A=Sommet("A",{})
+B=Sommet("B",{})
+C=Sommet("C",{})
+D=Sommet("D",{})
+A.initAdjacents({B:3,C:8})
+B.initAdjacents({A:3,C:5})
+C.initAdjacents({A:8,B:5})
+D.initAdjacents({A:12,B:1,D:1})
+#A.afficherAdjacents()
+#B.afficherAdjacents()
+D.afficherAdjacents()
+print(D.retourneDegre())
+print(int(D.retourneBoucle()))
