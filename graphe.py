5d3fb7714328366602b5884dd96f6fca0e92dff8
Mael-Le-Doare <mael.ledoare@etudiant.univ-rennes.fr>
2026-02-10 11:53:21 +0100

    Initianalisation des sommets du graphe

--- graphe.py
+++ graphe.py
@@ -1,14 +1,38 @@
 import sommet
 class Graphe: 
-    #la liste de tout les sommet
+    #la matrice a deux dimensions de tout les sommet
     sommets : list = []
     #nombre de Sommet:
     ordre : int = 0
 
     def __init__(self, ordre : int):
+        #creation de tout les sommets et initianalisation de l'ordre du graphe
+        self.initGraph(ordre)
 
-        for lettre in range(0):
-            for i in range(1,ordre-1):
-                sommet.append(sommet("lettre",{}))
+    
+    def initGraph(self, ordre : int):
+        #methode qui remplie la liste des sommets celon la convention classique du nommage des cases d'un plateau d'echec
+        ligne : list = []
+        for lettre in range(1,ordre+1):
+            for i in range(1,ordre+1):
+                #les sommet auront un nom du type : E4, A2, f7...
+                #on n'inisianalise pas encore leur sommets adjacents
+                ligne.append(sommet.Sommet(chr(lettre+64)+str(i),{}))
+                #actualise egalement l'ordre du graphe
+                self.ordre+=1
+            #on ajoute notre ligne
+            self.sommets.append(ligne)
+            #on vide notre liste
+            ligne=[]
 
+    def afficheSommets(self):
+        #affichage de tout les sommets du graphes
+        for i in self.sommets:
+            for j in i:
+                print(j)
+
+monGraphe = Graphe(8)
+monGraphe.afficheSommets()
+print("--------------------------")
+print(monGraphe.ordre)
 
