d87b1c8b1fd87557eff2b9f428de37dfc0163171
Viridian-exe <anatole.barbier@etudiant.univ-rennes.fr>
2026-02-10 17:01:13 +0100

    modified:   main.py

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ import constantes
 display = affichage.display(constantes.board,"plateau")
 display.draw_piece(entities.Position(2,1))
 display.run()
+
 display.draw_board()
 display.draw_piece(entities.Position(1,2))
